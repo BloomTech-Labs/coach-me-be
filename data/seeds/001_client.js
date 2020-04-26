6d9d4e24d20f71cf3461a0f727af88fd3ab88118
Jesse-Tingle <jtingle0@gmail.com>
2020-04-26 07:03:01 -0400

    sets up client seeds file

--- data/seeds/001_client.js
+++ data/seeds/001_client.js
@@ -1,13 +1,55 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function (knex) {
+	await knex("client").insert([
+		{
+			id: "d322ddbd-026d-4c29-8aea-fd7ae18ef572",
+			first_name: "George",
+			last_name: "Thomas",
+			email: "gthomas@gmail.com",
+			phone: 5023324040,
+			dob: "05-APR-1987",
+			password: "password123",
+		},
+		{
+			id: "82ad6337-b099-4bd7-b0f7-6c0c316fe250",
+			first_name: "Jesse",
+			last_name: "Tingle",
+			email: "tingle@gmail.com",
+			phone: 5024566832,
+			dob: "30-NOV-1990",
+			password: "password123",
+		},
+		{
+			id: "404e1e3c-2275-4a31-b073-123098307277",
+			first_name: "Brian",
+			last_name: "Taveras",
+			email: "btaveras@gmail.com",
+			phone: 4066589807,
+			dob: "03-JAN-1989",
+		},
+		{
+			id: "b1b76dd7-3e32-4fcd-8947-da9de089de35",
+			first_name: "Joshua",
+			last_name: "Burleson",
+			email: "burleson@gmail.com",
+			phone: 8596089870,
+			dob: "09-DEC-1987",
+			password: "password123",
+		},
+		{
+			id: "0d560384-5bce-46c9-94fb-5c5e8209f6dd",
+			first_name: "Kemberly",
+			last_name: "Eliscar",
+			email: "keliscar@gmailcom",
+			phone: 5024567894,
+			dob: "22-JUL-1992",
+		},
+		{
+			id: "46b97b6f-f3bf-494a-a840-44d3393d376f",
+			first_name: "Jarrod",
+			last_name: "Skahill",
+			email: "jkahill@gmail.com",
+			phone: 5024783254,
+			dob: "03-OCT-1990",
+		},
+	]);
 };
