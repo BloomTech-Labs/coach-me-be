c73a2efe44f05a080c45e28d9ed96b7e22d35a90
Jesse-Tingle <jtingle0@gmail.com>
2020-04-25 20:34:16 -0400

    sets up knex migration coach table

---
+++ data/migrations/20200425202110_coach.js
@@ -0,0 +1,18 @@
+exports.up = async function (knex) {
+	await knex.schema.createTable("coach", (tbl) => {
+		tbl.uuid("id").primary();
+		tbl.string("first_name", 20).notNull();
+		tbl.string("last_name", 20).notNull();
+		tbl.string("email").notNull();
+		tbl.integer("phone", 10).notNull();
+		tbl.string("password").notNull();
+		tbl.string("profile_pic_id");
+		tbl.string("creds_id");
+		table.timestamp("created_at").defaultTo(knex.fn.now());
+		tbl.timestamp("last_logged_in");
+	});
+};
+
+exports.down = async function (knex) {
+	await knex.schema.dropTableIfExists("coach");
+};
