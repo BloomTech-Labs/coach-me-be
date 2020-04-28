8140c2aacc274397f1dc5c9318ac6d9de79a968e
Jesse-Tingle <jtingle0@gmail.com>
2020-04-27 20:41:25 -0400

    sets up seeds for coach file

--- data/seeds/002_coach.js
+++ data/seeds/002_coach.js
@@ -1,13 +1,24 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function (knex) {
+	await knex("coach").insert([
+		{
+			id: "11e30af8-b531-49b2-9387-2647dc76444a",
+			first_name: "Derek",
+			last_name: "Peters",
+			email: "derekpeters@gmail.com",
+			phone: 5029876732,
+			password: "password123",
+			profile_pic_id: 1093,
+			creds_id: "123405",
+		},
+		{
+			id: "659918be-887a-4ce7-a5c7-29434aeb1cb7",
+			first_name: "Thomas",
+			last_name: "Barrett",
+			email: "thomasbarrett@gmail.com",
+			phone: 5067934783,
+			password: "password123",
+			profile_pic_id: 29384,
+			creds_id: 304958,
+		},
+	]);
 };
