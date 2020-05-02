c320d2c9585fe25638d4f908eb0afae8553cc0a5
brianetaveras <taveras574@gmail.com>
2020-05-02 19:35:27 -0400

    Turned coach auth error handlers into a middleware

---
+++ middleware/auth/coachAuthErrorHandler.js
@@ -0,0 +1,28 @@
+function coachAuthErrorHandler(){
+    return (req, res, next) =>{
+        const {first_name, last_name, email, phone, password, confirm_password, height, sex} = req.body;
+        // Regular expresion that tests if the password is strong enough
+        // ^ = String starts
+        // (?=.*[a-z]) contains any lowercase alphabetical char from a-z
+        // (?=.*[A-Z]) containps any uppercase alphabetical char from A-Z
+        // (?=.*[0-9]) contains any number
+        // (?=.*[!@#\$%\^&\*]) contains any special character
+        // (?=.{8,}) string length of at lest 8 characters (You can add a second number to have the length be between those)
+        const passRegex = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+        // All of this could probably move to a middleware later. Checking required fields are there,
+        // Checking if passwords match, checking if pass is strong enough. We could have Detailed error messages
+        // and do password checks for each field.
+        if (!first_name || !last_name || !email || !phone || !password || !confirm_password || !height|| !sex){
+            return res.status(400).json("All fields must be filled!");
+        }
+        if(password !== confirm_password){
+            return res.status(400).json("Passwords do not match.");
+        }
+        if(!passRegex.test(password)){
+            return res.status(400).json("Password must contain at least 8 characters, one upper case alphabetical character, and a number.");
+        }
+        next()
+    }
+}
+
+module.exports = coachAuthErrorHandler;
--- routes/auth/client-auth.js
+++ routes/auth/client-auth.js
@@ -3,37 +3,13 @@ const passport = require("passport");
 const Clients = require('../../models/client-model');
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken')
-router.post('/register', async (req, res, next)=>{
+router.post('/register', require("../../middleware/auth/coachAuthErrorHandler")(), async (req, res, next)=>{
     try {
-        const {first_name, last_name, email, phone, password, confirm_password, height, sex} = req.body;
-        // Regular expresion that tests if the password is strong enough
-        // ^ = String starts
-        // (?=.*[a-z]) contains any lowercase alphabetical char from a-z
-        // (?=.*[A-Z]) containps any uppercase alphabetical char from A-Z
-        // (?=.*[0-9]) contains any number
-        // (?=.*[!@#\$%\^&\*]) contains any special character
-        // (?=.{8,}) string length of at lest 8 characters (You can add a second number to have the length be between those)
-        
-        const passRegex = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-
-        // All of this could probably move to a middleware later. Checking required fields are there,
-        // Checking if passwords match, checking if pass is strong enough. We could have Detailed error messages
-        // and do password checks for each field.
-
-        if (!first_name || !last_name || !email || !phone || !password || !confirm_password || !height|| !sex){
-            return res.status(400).json("All fields must be filled!");
-        }
-        if(password !== confirm_password){
-            return res.status(400).json("Passwords do not match.");
-        }
-        if(!passRegex.test(password)){
-            return res.status(400).json("Password must contain at least 8 characters, one upper case alphabetical character, and a number.");
-        }
-        const user = await Clients.getUserByEmail(email);
+        const user = await Clients.getUserByEmail(req.body.email);
         if(user){
             return res.status(402).json("There is an account associated with your email address. Try loggin in.");
         }
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
         await Clients.addClient({
             ...req.body,
             password: hashedPassword
