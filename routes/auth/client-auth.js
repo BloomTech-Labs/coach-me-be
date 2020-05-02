6a70b61f13f57a0ed9d605e8bbe907ebaf9ebf39
brianetaveras <taveras574@gmail.com>
2020-05-02 19:42:25 -0400

    Changed auth error handler file name

--- routes/auth/client-auth.js
+++ routes/auth/client-auth.js
@@ -3,7 +3,7 @@ const passport = require("passport");
 const Clients = require('../../models/client-model');
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken')
-router.post('/register', require("../../middleware/auth/coachAuthErrorHandler")(), async (req, res, next)=>{
+router.post('/register', require("../../middleware/auth/clientAuthErrorHandler")(), async (req, res, next)=>{
     try {
         const user = await Clients.getUserByEmail(req.body.email);
         if(user){
@@ -14,14 +14,11 @@ router.post('/register', require("../../middleware/auth/coachAuthErrorHandler")(
             ...req.body,
             password: hashedPassword
         });
-
         return res.json('success')
-
     } catch (error) {
         next(error)
     }
 });
-
 router.post('/login', async (req, res, next)=>{
     passport.authenticate('local', (info, user, err) => {
         if (err){
@@ -30,14 +27,11 @@ router.post('/login', async (req, res, next)=>{
         if(user){
             return res.json('success')
         }
-
     })(req, res, next);
 });
-
 router.post('/logout', async (req, res, next)=>{
     console.log(req.signedCookies)
     return res.clearCookie('token').json('Logged out successfully.')
 });
-
 
 module.exports = router;
