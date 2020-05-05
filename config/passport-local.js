7ba84ed6204b6e5306b68d9b5becab9cfeb9a94a
brianetaveras <taveras574@gmail.com>
2020-05-05 19:57:58 -0400

    Local auth should be good to go. All user types answer to the same endpoit. (the auth master)

--- config/passport-local.js
+++ config/passport-local.js
@@ -7,17 +7,17 @@ function localLogin(passport) {
     new LocalStrategy(
       { usernameField: "email", passReqToCallback: true },
       async (req, email, password, done) => {
-        const user = await User.getUserByEmail(email, req.user_type);
+        const user = await User.getUserByEmail(email, req.query.user_type);
         if (!user) return done(null, false, { message: "User not found" });
         const passwordMatches = await bcrypt.compare(password, user.password);
         return passwordMatches ? done(null, user) : done(null, false, { message: "Your password is incorrect!" });
       }
     )
   );
+  
   // Serializies users and adds information to the session object.
   passport.serializeUser(function(user, done) {
-    console.log(user)
-    done(null, {id: user.id, type: 'client'});
+    done(null, {id: user.id, type: user.user_type});
   });
   // Deserialize users so we can view the information through
   // req.session.passort
--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,5 +1,63 @@
 const router = require('express').Router();
+const passport = require("passport");
+const bcrypt = require("bcrypt"); 
+const client_db = require('../../models/client-model');
+const coach_db = require('../../models/coach-models');
+const httpError = require('http-errors'); 
 
-router.use('/client', require('./client-auth'));
+router.post('/register', require("../../middleware/auth/RegisterErrorHandler")(), async (req, res, next)=>{
+    try {
+        const {user_type} = req.query;
+        const user = await client_db.getUserByEmail(req.body.email, user_type);
+        if(user) return res.status(402).json("There is an account associated with your email address. Try logging in.");
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        switch(user_type){
+            case 'client':
+                await client_db.addClient({
+                    ...req.body,
+                    password: hashedPassword
+                });
+                return res.json('success');
+            case 'coach':
+                await coach_db.addCoach({
+                    ...req.body,
+                    password: hashedPassword
+                })
+                return res.json('success');
+            default:
+                throw new httpError(400, "user_type query value must be provided. (e.g: /auth?user_type='coach'")
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+           
+
+router.post('/login', async (req, res, next) => {
+    try {
+        if(req.session?.passport?.user) return res.redirect(`/api/${req.query.user_type}/${req.session.passport.user.id}`);
+        passport.authenticate('local', {userProperty: 'email'},
+        (err, user, info) => {
+            if(!user) return res.json(info.message);
+            req.login(user, function(error) {
+                if (error) throw error;
+                res.json('Login successful');
+            });
+        })(req, res, next);    
+    } catch (error) {
+        next(error);
+        }
+    }
+);     
+router.post('/logout', async (req, res, next)=>{
+    try {
+        req.session.destroy();
+        return res.json('Logged out successfully.');
+        
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 module.exports = router;
