8a6d81a529c6d69708205af7301e200c6c3af77c
Jesse-Tingle <jtingle0@gmail.com>
2020-05-05 22:30:10 -0400

    fixes spelling errors in comments

--- config/passport-local.js
+++ config/passport-local.js
@@ -3,26 +3,28 @@ const User = require("../models/client-model");
 const bcrypt = require("bcrypt");
 
 function localLogin(passport) {
-  passport.use(
-    new LocalStrategy(
-      { usernameField: "email", passReqToCallback: true },
-      async (req, email, password, done) => {
-        const user = await User.getUserByEmail(email, req.query.user_type);
-        if (!user) return done(null, false, { message: "User not found" });
-        const passwordMatches = await bcrypt.compare(password, user.password);
-        return passwordMatches ? done(null, user) : done(null, false, { message: "Your password is incorrect!" });
-      }
-    )
-  );
-  
-  // Serializies users and adds information to the session object.
-  passport.serializeUser(function(user, done) {
-    done(null, {id: user.id, type: user.user_type});
-  });
-  // Deserialize users so we can view the information through
-  // req.session.passort
-  passport.deserializeUser(function(user, done) {
-    done(null, user);
-  });
+	passport.use(
+		new LocalStrategy(
+			{ usernameField: "email", passReqToCallback: true },
+			async (req, email, password, done) => {
+				const user = await User.getUserByEmail(email, req.query.user_type);
+				if (!user) return done(null, false, { message: "User not found" });
+				const passwordMatches = await bcrypt.compare(password, user.password);
+				return passwordMatches
+					? done(null, user)
+					: done(null, false, { message: "Your password is incorrect!" });
+			}
+		)
+	);
+
+	// Serializies users and adds information to the session object.
+	passport.serializeUser(function (user, done) {
+		done(null, { id: user.id, type: user.user_type });
+	});
+	// Deserialize users so we can view the information through
+	// req.session.passport
+	passport.deserializeUser(function (user, done) {
+		done(null, user);
+	});
 }
 module.exports = localLogin;
--- middleware/auth/RegisterErrorHandler.js
+++ middleware/auth/RegisterErrorHandler.js
@@ -1,32 +1,54 @@
-function clientAuthErrorHandler(type = 'client'){
-    return (req, res, next) =>{
-        const {first_name, last_name, email, phone, password, confirm_password, height, sex} = req.body;
-        // Regular expresion that tests if the password is strong enough
-        // ^ = String starts
-        // (?=.*[a-z]) contains any lowercase alphabetical char from a-z
-        // (?=.*[A-Z]) containps any uppercase alphabetical char from A-Z
-        // (?=.*[0-9]) contains any number
-        // (?=.*[!@#\$%\^&\*]) contains any special character
-        // (?=.{8,}) string length of at lest 8 characters (You can add a second number to have the length be between those)
-        const passRegex = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-        // All of this could probably move to a middleware later. Checking required fields are there,
-        // Checking if passwords match, checking if pass is strong enough. We could have Detailed error messages
-        // and do password checks for each field.
-        if(!height || !sex && type === 'client'){
-            return res.status(400).json("All fields must be filled!");
-        }
-        if (!first_name || !last_name || !email || !phone || !password || !confirm_password){
-            console.log('getting here?')
-            return res.status(400).json("All fields must be filled!");
-        }
-        if(password !== confirm_password){
-            return res.status(400).json("Passwords do not match.");
-        }
-        if(!passRegex.test(password)){
-            return res.status(400).json("Password must contain at least 8 characters, one upper case alphabetical character, and a number.");
-        }
-        next();
-    }
+function clientAuthErrorHandler(type = "client") {
+	return (req, res, next) => {
+		const {
+			first_name,
+			last_name,
+			email,
+			phone,
+			password,
+			confirm_password,
+			height,
+			sex,
+		} = req.body;
+		// Regular expression that tests if the password is strong enough
+		// ^ = String starts
+		// (?=.*[a-z]) contains any lowercase alphabetical char from a-z
+		// (?=.*[A-Z]) contains any uppercase alphabetical char from A-Z
+		// (?=.*[0-9]) contains any number
+		// (?=.*[!@#\$%\^&\*]) contains any special character
+		// (?=.{8,}) string length of at lest 8 characters (You can add a second number to have the length be between those)
+		const passRegex = new RegExp(
+			"(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+		);
+		// All of this could probably move to a middleware later. Checking required fields are there,
+		// Checking if passwords match, checking if pass is strong enough. We could have Detailed error messages
+		// and do password checks for each field.
+		if (!height || (!sex && type === "client")) {
+			return res.status(400).json("All fields must be filled!");
+		}
+		if (
+			!first_name ||
+			!last_name ||
+			!email ||
+			!phone ||
+			!password ||
+			!confirm_password
+		) {
+			console.log("getting here?");
+			return res.status(400).json("All fields must be filled!");
+		}
+		if (password !== confirm_password) {
+			return res.status(400).json("Passwords do not match.");
+		}
+		if (!passRegex.test(password)) {
+			return res
+				.status(400)
+				.json(
+					"Password must contain at least 8 characters, one upper case alphabetical character, and a number."
+				);
+		}
+		next();
+	};
 }
 
 module.exports = clientAuthErrorHandler;
