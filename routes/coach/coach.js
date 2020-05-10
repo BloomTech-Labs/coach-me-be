8abcd03757fa3aaf54a51e4858c202bbf3a8ba06
Jesse-Tingle <jtingle0@gmail.com>
2020-05-09 21:47:16 -0400

    fixes import error with access middleware

--- routes/coach/coach.js
+++ routes/coach/coach.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const coachDB = require("../../models/coach-models");
 const helper = require("../../utils/coachMeHelpers");
+const access = require("../../middleware/auth/globalPriv");
 
 /* MIDDLEWARE */
 router.use("/:id", require("../../middleware/pathValidator").checkID);
+// router.use(access.protected)
+router.use("/:id", access.private);
 
 /*  
 	'/coach'
