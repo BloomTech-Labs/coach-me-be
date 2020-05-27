290386ef93c497b3c4d331a59baa716e33603183
Joshua Burleson <52901917+Joshua-Burleson@users.noreply.github.com>
2020-05-26 21:00:54 -0400

    Code Climate Fix
    Code Climate Fix

--- routes/coach/coach.js
+++ routes/coach/coach.js
@@ -31,8 +31,7 @@ router.get("/", async (req, res, next) => {
 */
 router.get("/:id", async (req, res, next) => {
   try {
-    const id =
-      req.params.id === "me" ? req.session?.passport?.user?.id : req.params.id;
+    const id = req.params.id;
     const profile = {
       ...(await coachDB.getUserById(id, "coach")),
       password: null,
