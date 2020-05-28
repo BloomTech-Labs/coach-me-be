bd924977684c1daf589d8999f2a60f963f16094e
Joshua Burleson <josh.burleson@bytes.co>
2020-05-27 22:18:40 -0400

    Creates session verification endpoint for react protected routes

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -173,6 +173,6 @@ router.post(
 	}
 );
 
-router.get('/verify_session', access.protected, (req, res) => res.status(200).send('Okay'));
+router.get('/verify_session', access.protected, (req, res) => res.status(200).json('Okay'));
 
 module.exports = router;
