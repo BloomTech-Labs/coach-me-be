89955bdf90720c67347e3699660644eaff509df9
Jesse-Tingle <jtingle0@gmail.com>
2020-06-15 20:53:37 -0400

    troubleshooting new tables for conditions on dashboard

---
+++ data/seeds/conditions.js
@@ -0,0 +1,24 @@
+exports.seed = async function (knex) {
+	await knex("conditions").insert([
+		{
+			id: "d322ddbd-026d-4c29-8aea-fd7ae18ef572",
+			condition: "Diabetes",
+		},
+		{
+			id: "7a27dd43-b1d1-47aa-8d62-b6f63a5a2e29",
+			condition: "HBP",
+		},
+		{
+			id: "99293a83-cb81-4be9-bacc-1f17c1191d5e",
+			condition: "Anxiety",
+		},
+		{
+			id: "b48e1d3e-f976-44bf-8d16-c164d81adb89",
+			condition: "High Cholesterol",
+		},
+		{
+			id: "b48e1d3e-f976-44bf-8d16-c164d81adb89",
+			condition: "Depression",
+		},
+	]);
+};
