610a2b755ada7e4d748cab285d23ed34b68b9ea6
Jesse-Tingle <jtingle0@gmail.com>
2020-06-18 20:26:41 -0400

    fixes error with client conditions in seed files

--- data/seeds/007_client_condition.js
+++ data/seeds/007_client_condition.js
@@ -2,43 +2,19 @@ exports.seed = async function (knex) {
 	await knex("client_condition").insert([
 		{
 			client_id: "d322ddbd-026d-4c29-8aea-fd7ae18ef572",
-			condition_id: "d322ddbd-026d-4c29-8aea-fd7ae18ef572",
-		},
-		{
-			client_id: "d322ddbd-026d-4c29-8aea-fd7ae18ef572",
-			condition_id: "7a27dd43-b1d1-47aa-8d62-b6f63a5a2e29",
-		},
-		{
-			client_id: "d322ddbd-026d-4c29-8aea-fd7ae18ef572",
-			condition_id: "b48e1d3e-f976-44bf-8d16-c164d81adb89",
+			condition_id: "12b228ba-9abd-490f-b555-79eb872dd024",
 		},
 		{
 			client_id: "82ad6337-b099-4bd7-b0f7-6c0c316fe250",
-			condition_id: "7a27dd43-b1d1-47aa-8d62-b6f63a5a2e29",
+			condition_id: "3acecb61-11d3-4df4-a0e9-9c3ea1b03dd3",
 		},
 		{
 			client_id: "404e1e3c-2275-4a31-b073-123098307277",
-			condition_id: "b48e1d3e-f976-44bf-8d16-c164d81adb89",
+			condition_id: "5b929b97-50d9-44bf-a83c-dc3ec348a96d",
 		},
 		{
-			client_id: "b1b76dd7-3e32-4fcd-8947-da9de089de35",
-			condition_id: "7a27dd43-b1d1-47aa-8d62-b6f63a5a2e29",
-		},
-		{
-			client_id: "0d560384-5bce-46c9-94fb-5c5e8209f6dd",
-			condition_id: "99293a83-cb81-4be9-bacc-1f17c1191d5e",
-		},
-		{
-			client_id: "0d560384-5bce-46c9-94fb-5c5e8209f6dd",
-			condition_id: "b48e1d3e-f976-44bf-8d16-c164d81adb89",
-		},
-		{
-			client_id: "46b97b6f-f3bf-494a-a840-44d3393d376f",
-			condition_id: "99293a83-cb81-4be9-bacc-1f17c1191d5e",
-		},
-		{
-			client_id: "46b97b6f-f3bf-494a-a840-44d3393d376f",
-			condition_id: "d322ddbd-026d-4c29-8aea-fd7ae18ef572",
+			client_id: "404e1e3c-2275-4a31-b073-123098307277",
+			condition_id: "10544fb3-dc97-429f-9899-4c6d2c80c604",
 		},
 	]);
 };
