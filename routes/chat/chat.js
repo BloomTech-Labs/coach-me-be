45050577e91737530cac876ab34ae97404fc27fb
Joshua Burleson <josh.burleson@bytes.co>
2020-06-22 22:43:49 -0400

    Adds chat to backend

---
+++ routes/chat/chat.js
@@ -0,0 +1,35 @@
+const app = require('express')();
+const PORT = process.env.PORT || 5000;
+const base64id = require('base64id');
+
+const  chatClient = app => {
+    const io = require('socket.io')(app);
+    const users = {};
+    io.engine.generateId = req => base64id.generateId();
+
+    io.on('connection', socket => {
+        if (!users[socket.id]) {
+            users[socket.id] = socket.id;
+        }
+        console.log(users);
+
+        socket.emit("init", socket.id);
+
+        socket.emit("usersOnline", users);
+
+        socket.on('disconnect', () => {
+            delete users[socket.id];
+        });
+
+        socket.on("callUser", (data) => {
+            io.to(data.userToCall).emit('callRequest', {signal: data.signalData, from: data.from});
+        })
+
+        socket.on("acceptCall", (data) => {
+            io.to(data.to).emit('callAccepted', data.signal);
+        })
+    });
+}
+
+
+module.exports = chatClient;
